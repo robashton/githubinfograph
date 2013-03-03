af723253f0121b4a8dec11b8a3439478c29ebc8d
Rob Ashton <robashton@codeofrob.com>
2013-03-03 21:03:09 +0000

    Basic no projections

--- projections/count_of_paranoid_pushes.js
+++
@@ -1,9 +0,0 @@
-fromStream('github-paranoidpushes')
-   .when({
-     $init: function() {
-        return { count: 0 }
-     },
-     ParanoidPush: function(state, ev) {
-        state.count++
-     }
-   })
